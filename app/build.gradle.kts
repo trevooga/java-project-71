2f05e6abe91fa918383fa89f61e3f3ec4b17ba12
LobotomyKuro <nojji@yandex.ru>
2024-07-15 16:55:25 +0300

    MVP project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "org.example"
@@ -8,12 +9,18 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
+application {
+    mainClass.set("hexlet.code.App")
+}
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("junit:junit:4.13.2")
+    implementation("info.picocli:picocli:4.7.6")
+
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
