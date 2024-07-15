0c87aa523eba33a5f61089ef766d9229213fb75c
LobotomyKuro <nojji@yandex.ru>
2024-07-15 19:22:06 +0300

    picocli imported

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,23 @@
 package hexlet.code;
+import hexlet.code.Differ;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import  picocli.CommandLine.Parameters;
 
-public class App {
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+
+public class App implements Runnable {
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int exitCode = new CommandLine(new App()).execute(args);
+        if (exitCode != 0) {
+            System.exit(exitCode);
+        }
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello world!");
     }
 }
