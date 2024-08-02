2833f5eeab661b0b8080e452795b7374f3959256
trevooga <nojji@yandex.ru>
2024-08-02 19:11:57 +0300

    added Differ

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,6 +1,55 @@
 package hexlet.code;
 
 
-public class Differ {
+import utils.Parsing;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+public class Differ {
+    public static List<String> difference(File file1, File file2) {
+        List<String> differenceOfFiles = new ArrayList<>();
+        try {
+            Map<String, Object> mapToDifference1 = Parsing.parse(file1);
+            Map<String, Object> mapToDifference2 = Parsing.parse(file2);
+
+            // Проходим по элементам первого файла
+            for (Map.Entry<String, Object> entry1 : mapToDifference1.entrySet()) {
+                String key1 = entry1.getKey();
+                Object value1 = entry1.getValue();
+
+                if (mapToDifference2.containsKey(key1)) {
+                    Object value2 = mapToDifference2.get(key1);
+                    if (!value1.equals(value2)) {
+                        differenceOfFiles.add("- " + key1 + ": " + value1);
+                        differenceOfFiles.add("+ " + key1 + ": " + value2);
+                    } else {
+                        differenceOfFiles.add(key1 + ": " + value2);
+                    }
+                } else {
+                    differenceOfFiles.add("- " + key1 + ": " + value1);
+                }
+            }
+
+            // Проходим по элементам второго файла, которых нет в первом
+            for (Map.Entry<String, Object> entry2 : mapToDifference2.entrySet()) {
+                String key2 = entry2.getKey();
+                if (!mapToDifference1.containsKey(key2)) {
+                    differenceOfFiles.add("+ " + key2 + ": " + entry2.getValue());
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка при чтении файлов: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Произошла ошибка: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return differenceOfFiles.stream().sorted().toList();
+    }
 }
--- app/src/main/java/utils/Parsing.java
+++ app/src/main/java/utils/Parsing.java
@@ -7,10 +7,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 
-public class WatchTheFile {
+public class Parsing {
     public static Map<String, Object> parse(File jsonfile) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.readValue(jsonfile, new TypeReference<>() {
