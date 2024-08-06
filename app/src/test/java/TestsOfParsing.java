2f253133aac56ef2ad0042bb5a430f37bed5716e
trevooga <nojji@yandex.ru>
2024-08-06 20:16:35 +0300

    reworked style in tests

--- app/src/test/java/TestsOfParsing.java
+++ app/src/test/java/TestsOfParsing.java
@@ -10,16 +10,23 @@ public class TestsOfParsing {
 
     @Test
     public void parsingTest() throws IOException {
-        String correctFile = "{\n" +
-                "- follow: false\n" +
-        "  host: hexlet.io\n" +
-                "- proxy: 123.234.53.22\n" +
-                "- timeout: 50\n" +
-                "+ timeout: 20\n" +
-                "+ verbose: true\n" +
-"}";
+        String correctFile = "{\n"
+                +
+                "- follow: false\n"
+                +
+                "  host: hexlet.io\n"
+                +
+                "- proxy: 123.234.53.22\n"
+                +
+                "- timeout: 50\n"
+                +
+                "+ timeout: 20\n"
+                +
+                "+ verbose: true\n"
+                +
+                "}";
         File file1 = new File("src/test/resources/file1.json");
         File file2 = new File("src/test/resources/file2.json");
-        assertEquals(correctFile , Differ.generate(file1, file2));
+        assertEquals(correctFile, Differ.generate(file1, file2));
     }
 }
