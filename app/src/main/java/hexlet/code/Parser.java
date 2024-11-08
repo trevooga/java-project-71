978a4c16c54893bb52282d09fcffc84e95a2604e
trevoga <nojji@yandex.ru>
2024-11-08 10:21:36 +0300

    added yml format

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -13,6 +13,8 @@ public class Parser {
     public static Map<String, Object> parse(String text, String formatName) throws Exception {
         return switch (formatName) {
             case "json" -> jsonMap(text);
+            case "yml" -> yamlMap(text);/*сделал для hexlet check, тк он подает
+                                                        на вход yml формат и выбрасывается исключение*/
             case "yaml" -> yamlMap(text);
             default -> throw new Exception("Unknown format:" + formatName);
         };
